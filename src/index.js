fe5da5ef5824beb8bb3913d170c4ef6eb7d04739
kylejb <47502769+kylejb@users.noreply.github.com>
2020-08-09 22:20:17 -0400

    Deliverable: Step 3

--- src/index.js
+++ src/index.js
@@ -13,12 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const renderDogsForBar = async () => {
         let dogs = await getDogs();
-
-        dogs.forEach(dog => renderDogOnBar(dog));
+        console.log(dogs)
+        dogs.forEach(dog => {
+            renderDogOnBar(dog);
+            generateDogFrags(dog);
+        });
 
     }
 
-    const dogBarDiv = document.getElementById("dog-bar")
+    const dogBarDiv = document.getElementById("dog-bar");
+
     const renderDogOnBar = (dogObj) => {
         const span = document.createElement("span");
         span.classList.add("dog-navbar-card")
@@ -26,9 +30,61 @@ document.addEventListener("DOMContentLoaded", () => {
         dogBarDiv.appendChild(span);
     }
 
+    const dogInfoDiv = document.getElementById("dog-info");
+    const dogShowFrag = new DocumentFragment();
+
+    const generateDogFrags = (dogObj) => {
+        // isGoodDog Helper function
+        const isGoodDogBtn = () => {
+            if (dogObj.isGoodDog){
+                return "Bad Dog!";
+            } else {
+                return "Good Dog!";
+            }
+        }
+        const dogShowDiv = document.createElement("div");
+        dogShowDiv.classList.add("dog-show-card");
+        dogShowFrag.appendChild(dogShowDiv);
+        dogShowDiv.innerHTML = `
+            <img src=${dogObj.image} />
+            <h2>${dogObj.name}</h2>
+            <button>${isGoodDogBtn()}</button>
+        `
+        // dogInfoDiv.appendChild(dogShowDiv);
+    }
+
+    const dogShowHandler = () => {
+
+        // helper method to filter fragment docs for fun
+        // alternative to CSS hidden attribute
+        const filterByDogName = (target) => {
+            for (const dog of dogShowFrag.children) {
+                const dogShowCard = dog.innerHTML.replace(/^\s+|\s+$/g, "");
+
+                const regex = new RegExp(target, 'gi'); 
+                // debugger;
+                if (dogShowCard.match(regex) && dogShowCard.match(regex)[0] === target) {
+                    console.log(dogShowCard.match(regex)[0].index);
+                    return dogShowCard;
+                } else {
+                    console.log("nope");
+                }
+            }
+        }
+        const dogBarDiv = document.getElementById("dog-bar");
+
+        dogBarDiv.addEventListener("click", e => {
+            let dogShowCard = filterByDogName(e.target.textContent);
+        
+            if (dogShowCard) {
+                const container = document.getElementById("dog-info");
+                container.innerHTML = dogShowCard
+            }
+        })
+    }
 
 
     renderDogsForBar();
-
+    dogShowHandler();
 
 })
